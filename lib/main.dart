2b606a414a7bd4e68dc1310fdd0b659f7d7b46cb
sheirnass <shernassain@gmail.com>
2022-10-08 23:25:31 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:bankui/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Myapp());
+}
+
+class Myapp extends StatelessWidget {
+  const Myapp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      home: Home_page(),
+    );
+  }
+}
---
+++ lib/pages/containers.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Item_containers extends StatelessWidget {
+  final String imagepath;
+  final String title;
+  const Item_containers(
+      {Key? key, required this.imagepath, required this.title})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final Size screensize = MediaQuery.of(context).size;
+    return Padding(
+      padding: const EdgeInsets.all(12),
+      child: Column(
+        children: [
+          Container(
+            width: screensize.width * 0.1,
+            height: screensize.width * 0.1,
+            decoration: BoxDecoration(
+                color: Colors.transparent,
+                image: DecorationImage(
+                    image: AssetImage(imagepath), fit: BoxFit.cover)),
+          ),
+          SizedBox(
+            height: screensize.width * 0.05,
+          ),
+          Text(
+            title,
+            style: TextStyle(
+                color: Colors.white, fontWeight: FontWeight.bold, fontSize: 13),
+          ),
+        ],
+      ),
+    );
+  }
+}
